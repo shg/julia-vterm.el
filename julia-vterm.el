a19c0c4917c4663ad094de447f643db25904a7f6
shg <55463+shg@users.noreply.github.com>
2022-08-13 23:06:42 +0900

    Make julia-vterm-repl-program buffer-local

--- julia-vterm.el
+++ julia-vterm.el
@@ -64,7 +64,7 @@
   "A major mode for inferior Julia REPL."
   :group 'julia)
 
-(defvar julia-vterm-repl-program "julia"
+(defvar-local julia-vterm-repl-program "julia"
   "Name of the command for executing Julia code.
 Maybe either a command in the path, like julia
 or an absolute path name, like /usr/local/bin/julia
